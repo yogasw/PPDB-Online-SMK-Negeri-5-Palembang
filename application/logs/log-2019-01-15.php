f6ccecd963d27b9d3e39a1d178ebe9979f2af480
Yoga Setiawan <mryoga1995@gmail.com>
2019-01-15 11:31:54 +0700

    Soal fix, selanjutnya jawaban simpan di db

---
+++ application/logs/log-2019-01-15.php
@@ -0,0 +1,73 @@
+ERROR - 2019-01-15 04:13:20 --> ________________________________________________ START LOG ________________________________________________
+ERROR - 2019-01-15 04:13:20 --> ________________________________________________  REQUEST   ________________________________________________
+ERROR - 2019-01-15 04:13:20 --> Array
+(
+[Host] => arioki.web
+[Connection] => keep-alive
+[Content-Length] => 8
+[Accept] => */*
+[Origin] => http://arioki.web
+[X-Requested-With] => XMLHttpRequest
+[User-Agent] => Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36
+[Content-Type] => application/x-www-form-urlencoded; charset=UTF-8
+[Referer] => http://arioki.web/pmb/quizzes
+[Accept-Encoding] => gzip, deflate
+[Accept-Language] => id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7
+[Cookie] => ci_session=a5klcutabplc3315t824c7a0o01o6mls
+)
+
+ERROR - 2019-01-15 04:13:20 --> POST = Array
+(
+[11] => a
+[6] => a
+)
+
+ERROR - 2019-01-15 04:13:20 --> ________________________________________________  REPONSE   ________________________________________________
+ERROR - 2019-01-15 04:13:20 --> Array
+(
+[X-Powered-By] => PHP/5.6.25
+[Expires] => Thu, 19 Nov 1981 08:52:00 GMT
+[Cache-Control] => no-store, no-cache, must-revalidate, post-check=0, pre-check=0
+[Pragma] => no-cache
+[Set-Cookie] => ci_session=a5klcutabplc3315t824c7a0o01o6mls; expires=Tue, 15-Jan-2019 06:13:20 GMT; Max-Age=7200; path=/; httponly
+)
+
+ERROR - 2019-01-15 04:13:20 --> ________________________________________________  END LOG  ________________________________________________
+ERROR - 2019-01-15 04:16:51 --> 11
+ERROR - 2019-01-15 04:16:51 --> a
+ERROR - 2019-01-15 04:16:51 --> 6
+ERROR - 2019-01-15 04:16:51 --> a
+ERROR - 2019-01-15 04:17:06 --> 11
+ERROR - 2019-01-15 04:17:06 --> a
+ERROR - 2019-01-15 04:17:06 --> 6
+ERROR - 2019-01-15 04:17:06 --> a
+ERROR - 2019-01-15 04:22:39 --> Array
+(
+[0] =>
+[1] => 11:a
+[2] => 6:a
+)
+
+ERROR - 2019-01-15 04:23:52 --> Severity: Warning --> array_push() expects parameter 1 to be array, null given D:\data\belajar\WEB\skripsi\application\controllers\Ajax.php 228
+ERROR - 2019-01-15 04:23:52 --> Severity: Warning --> array_push() expects parameter 1 to be array, null given D:\data\belajar\WEB\skripsi\application\controllers\Ajax.php 228
+ERROR - 2019-01-15 04:23:52 -->
+ERROR - 2019-01-15 04:25:17 --> Array
+(
+[0] => 11:a
+[1] => 6:a
+)
+
+ERROR - 2019-01-15 04:25:34 --> Array
+(
+[0] => 11:a
+[1] => 6:a
+)
+
+ERROR - 2019-01-15 04:25:59 --> Array
+(
+[0] => 7:a
+[1] => 3:a
+)
+
+ERROR - 2019-01-15 04:27:48 --> 7:a,3:a
+ERROR - 2019-01-15 04:29:37 --> 7:a,3:a
