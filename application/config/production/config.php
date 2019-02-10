3adf740527492c993e3de4cc871d3f38220ed996
Yoga Setiawan <mryoga1995@gmail.com>
2019-02-10 11:42:36 +0700

    auto https config production

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -100,7 +100,7 @@ $config['charset'] = 'UTF-8';
 | setting this variable to TRUE (boolean).  See the user guide for details.
 |
 */
-$config['enable_hooks'] = FALSE;
+$config['enable_hooks'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -522,3 +522,5 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
