db31c4cf74b70739492ed16725d781e4412fb99c
Yoga Setiawan <mryoga1995@gmail.com>
2019-02-13 14:55:30 +0700

    ganti icon

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo(base_url()) ?>assets/img/apple-icon.png"/>
-    <link rel="icon" type="image/png" href="<?php echo(base_url()) ?>assets/img/favicon.png"/>
+    <link rel="icon" type="image/png" href="<?php echo(base_url()) ?>assets/img/favicon.ico"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <title>PPDB SMK N 5 PALEMBANG</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
